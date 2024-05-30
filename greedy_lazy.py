96b42263f2f53cac516b0e257324386c27158449
VCruzSant <sviniantiago@gmail.com>
2024-05-30 20:28:08 -0300

    Greedy and Lazy

--- greedy_lazy.py
+++ greedy_lazy.py
@@ -11,5 +11,8 @@ text = '''
 <p>Frase 1</p> <p>Frase 2</p> <p>Frase 4</p> <div>Frase 4</div>
 '''
 
+# all info together
 print(re.findall(r'<[pdiv]{1,3}>.*<\/[pdiv]{1,3}>', text))
+
+# A info per time
 print(re.findall(r'<[pdiv]{1,3}>.*?<\/[pdiv]{1,3}>', text))
