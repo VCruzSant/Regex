bf5b331c67c1ae159a04ae3a7c6edc857d071daa
VCruzSant <sviniantiago@gmail.com>
2024-05-30 20:27:59 -0300

    Greedy and Lazy

---
+++ greedy_lazy.py
@@ -0,0 +1,15 @@
+import re
+
+# Meta caracteres: ^ $ ( )
+# * -> 0 ou n
+# + -> 1 ou n
+# ? 0 ou 1
+# {n}
+# {min, max}
+
+text = '''
+<p>Frase 1</p> <p>Frase 2</p> <p>Frase 4</p> <div>Frase 4</div>
+'''
+
+print(re.findall(r'<[pdiv]{1,3}>.*<\/[pdiv]{1,3}>', text))
+print(re.findall(r'<[pdiv]{1,3}>.*?<\/[pdiv]{1,3}>', text))
